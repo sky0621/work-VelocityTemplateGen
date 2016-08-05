d4bc3f759273539da7a352c3c28dcca54083ced8
S.S <ss@ss.com>
2016-08-06 08:13:32 +0900

    つくりはじめ

---
+++ src/main/java/xyz/skycat/work/VelocityTemplateGen/Config.java
@@ -0,0 +1,11 @@
+package xyz.skycat.work.VelocityTemplateGen;
+
+/**
+ * Created by SS on 2016/08/06.
+ */
+public class Config {
+    public static String noRow = "A";
+
+    public static String vmNameCell = "G2";
+
+}
---
+++ src/main/java/xyz/skycat/work/VelocityTemplateGen/Main.java
@@ -0,0 +1,10 @@
+package xyz.skycat.work.VelocityTemplateGen;
+
+/**
+ * Created by SS on 2016/08/06.
+ */
+public class Main {
+    public static void main(String... args) {
+        // TODO
+    }
+}
